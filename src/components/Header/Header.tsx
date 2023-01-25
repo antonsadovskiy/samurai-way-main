644ff4d0893304c492da3760fa28dc6db384d851
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-25 23:55:20 +0300

    add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
+
+export default Header;
