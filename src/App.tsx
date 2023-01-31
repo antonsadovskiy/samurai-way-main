3aa122e06c67b81ff4a82c0225589dd933b04145
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-31 12:50:03 +0300

    create navbar and profile pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
-import Technologies from "./components/Technologies/Technologies";
+import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
-  return (
-    <div className="App">
-        <Header/>
-        <Technologies/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className="contentContainer">
+                <Navbar />
+                <Profile />
+            </div>
+        </div>
+    );
 }
 
 export default App;
