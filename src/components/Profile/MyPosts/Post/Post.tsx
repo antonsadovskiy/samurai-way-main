bb9ea7bce9ee3e4bcdb00c4133d7520c1db52757
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-01-31 16:25:14 +0300

    created type of post component & added props

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {FC} from 'react';
 import set from "./Post.module.css";
 import avatar from "../../../../asssets/images/ava.jpg"
 
+type PostPropsType = {
+    message: string,
+    likes: number,
+}
 
-const Post = () => {
+const Post: FC<PostPropsType> = (props) => {
     return (
         <div className={set.item}>
             <img src={avatar} alt="avatar"/>
             <span>
-                post 1
+                {props.message}
             </span>
-            <div>
+            <div className={set.likesContainer}>
                 <button>Like</button>
+                <span>{props.likes}</span>
             </div>
         </div>
     );
