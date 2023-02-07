e005a962d70bda9696ed61be8998e1898c49ed38
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-07 17:06:54 +0300

    added browser router to app, five routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,29 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header />
-            <div className="contentContainer">
-                <Navbar />
-                <Profile />
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+                <div className="mainContainer">
+                    <Navbar />
+                    <div className="contentContainer">
+                        <Route path="/profile" component={Profile} />
+                        <Route path="/dialogs" component={Dialogs} />
+                        <Route path="/news" component={News} />
+                        <Route path="/music" component={Music} />
+                        <Route path="/settings" component={Settings} />
+                    </div>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
