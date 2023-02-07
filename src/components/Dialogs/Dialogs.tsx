7c5da7d93f7ddcd3a518eb56e1ee30432c846086
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-07 17:07:44 +0300

    created dialogs, news, music, settings components

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import s from './Dialogs.module.css'
+
+const Dialogs:FC = (props) => {
+    return (
+        <div>
+            Dialogs
+        </div>
+    );
+};
+
+export default Dialogs;
