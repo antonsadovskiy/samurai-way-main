0080f05e74b3360109109d8d33593ada2349b6fb
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-17 23:52:53 +0300

    added addPost function

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state from "./redux/state";
+import state, {addPost} from "./redux/state";
 
-ReactDOM.render(<App state={state} />, document.getElementById('root'));
+
+ReactDOM.render(<App state={state} addPost={addPost}/>, document.getElementById('root'));
