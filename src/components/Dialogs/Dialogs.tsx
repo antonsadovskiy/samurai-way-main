a6666bcb4508ccd3a7cee5e906f2f711db7ce062
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-18 00:30:41 +0300

    created Messages.tsx, NewMessage.tsx

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,30 +1,18 @@
-import React, {createRef, FC, RefObject} from 'react';
+import React, {FC} from 'react';
 import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import {DialogPageType} from "../../redux/state";
-import UserMessageItem from "./UserMessageItem/UserMessageItem";
-import FriendMessageItem from "./FriendMessageItem/FriendMessageItem";
+import NewMessage from "./NewMessage/NewMessage";
+import Messages from "./Messages/Messages";
 
 type DialogsPropsType = {
     dialogsPage: DialogPageType
 }
 
-export type MessageItemPropsType = {
-    message: string
-}
-
 const Dialogs:FC<DialogsPropsType> = ({dialogsPage}) => {
 
     const dialogsItems = dialogsPage.dialogs.map( dialog => <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} avatar={dialog.avatar}/>)
-    const userMessageItems = dialogsPage.userMessages.map( message => <UserMessageItem key={message.id} message={message.message} />)
-    const friendMessageItems = dialogsPage.friendMessages.map( message => <FriendMessageItem key={message.id} message={message.message} />)
 
-    const addMessageTextarea:RefObject<HTMLTextAreaElement> = createRef()
-
-    const onClickHandler = () => {
-        const text = addMessageTextarea.current?.value
-        alert(text)
-    }
 
     return (
         <div>
@@ -33,14 +21,8 @@ const Dialogs:FC<DialogsPropsType> = ({dialogsPage}) => {
                     {dialogsItems}
                 </div>
                 <div className={s.messageItems}>
-                    <div className={s.messages}>
-                        <div className={s.friendMessages}>{friendMessageItems}</div>
-                        <div className={s.userMessages}>{userMessageItems}</div>
-                    </div>
-                    <div className={s.newMessage}>
-                        <textarea ref={addMessageTextarea}/>
-                        <button className={s.button} onClick={onClickHandler}>Send</button>
-                    </div>
+                    <Messages dialogsPage={dialogsPage}/>
+                    <NewMessage />
                 </div>
             </div>
         </div>
